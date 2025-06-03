1b1595b68fd5ab334f16ec392e7bcadfa3ad233f
markus1bauer <markus1.bauer@tum.de>
2025-06-03 13:51:20 +0200

    Update CWMs

--- R/_prepare_data.R
+++ R/_prepare_data.R
@@ -35,15 +35,8 @@ rm(list = ls())
 
 
 
-species <- read_csv(
-  here("data", "raw", "data_processed_species_plants_plot_20240813.csv"),
-  col_names = TRUE, na = c("na", "NA", ""), col_types = cols(
-    .default = "?"
-  )
-)
-
 sites <- read_csv(
-  here("data", "raw", "data_processed_environment_nms_20250306.csv"),
+  here("data", "raw", "data_processed_environment_nms_20250603.csv"),
   col_names = TRUE, na = c("na", "NA", ""), col_types = cols(
     .default = "?",
     eco.id = "f",
@@ -98,10 +91,6 @@ rm(list = setdiff(ls(), c(
 
 
 data <- sites %>%
-  mutate(
-    history = if_else(str_detect(history, "ie Fläche"), NA, history),
-    history = as.numeric(history)
-    ) %>%
   filter(
     eco.id %in% c(664, 654, 686) &
       !(eco.id == 686 & region == "centre") &
@@ -111,14 +100,13 @@ data <- sites %>%
 sites_esy16 <- data %>%
   select(
     id.plot, id.site, longitude, latitude, region, eco.id, eco.name, obs.year,
-    esy16,
-    site.type, history, hydrology, land.use.hist, fertilized, freq.mow,
+    esy16, site.type, hydrology, fertilized, mngm.type,
     cwm.abu.sla, cwm.abu.height, cwm.abu.seedmass,
     cwm.pres.sla, cwm.pres.height, cwm.pres.seedmass
   ) %>%
   group_by(
-    id.site, region, eco.id, eco.name, obs.year, esy16, site.type, history,
-    hydrology, land.use.hist, fertilized, freq.mow
+    id.site, region, eco.id, eco.name, obs.year, esy16, site.type,
+    hydrology, fertilized, mngm.type,
     ) %>%
   summarize(
     cwm.abu.sla.mean = mean(cwm.abu.sla),
@@ -133,8 +121,7 @@ sites_esy16 <- data %>%
 sites_esy4 <- data %>%
   select(
     id.plot, id.site, longitude, latitude, region, eco.id, eco.name, obs.year,
-    esy4, esy16,
-    site.type, history, hydrology, land.use.hist, fertilized, freq.mow,
+    esy4, site.type, hydrology, fertilized, mngm.type,
     cwm.abu.sla, cwm.abu.height, cwm.abu.seedmass,
     cwm.pres.sla, cwm.pres.height, cwm.pres.seedmass
   ) %>%
